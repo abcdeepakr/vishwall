4b259dd2205b904d9c5f37058d2518e47d74f865
deepakR-28 <awsmyo1234@gmail.com>
2021-06-17 00:06:33 +0530

    displayWish

--- src/Components/birthdayWishDisplay/birthdayWishDisplay.js
+++ src/Components/birthdayWishDisplay/birthdayWishDisplay.js
@@ -29,7 +29,8 @@ const BirthdayWishDisplay = (props) =>{
     else{
         wishes = ""
     }
-    console.log(props)
+    console.log(data)
+    console.log("[ERROR : ]"+ error)
     return (
         <div>
             {wishes ? <h1>Happy Birthday {props.match.params.undefined} Your friends have sent some lovely wishes</h1> :
